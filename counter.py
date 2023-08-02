ba615a47654151bb3dbc1082b0b7333e30a4a418
Aibekchka <140369809+Aibekchka@users.noreply.github.com>
2023-08-02 11:43:37 +0600

    Add files via upload

---
+++ counter.py
@@ -0,0 +1,52 @@
+# COUNTER TASK 1
+result = 0
+a = int(input('Введите число'))
+for i in range(1,a+1):
+    result +=i
+print(result)
+
+
+# COUNTER AND LIST TASK 3
+result = 0
+a = int(input('Введите число'))
+for i in range(1,a+1):
+    result +=i
+print(result)
+
+counter = int(input())
+while counter > 0:
+     if counter % 2!=0:
+          print(counter)
+     counter = counter -1
+
+
+result = 0
+esep = int(input())
+for i in range(esep+1):
+    result +=i
+print(result)
+
+
+san = int(input())
+count = 1
+while count < san+1:
+     if count % 2 == 0:
+          print(count)
+     count = count + 1
+
+
+san = int(input())
+count = 2
+while count <= san:
+     if count % 2 == 0:
+          print(count)
+     count = count + 2
+
+
+languages = ['Java','Python','C++','Kottin']
+count = ''
+for i in languages:
+     count = count + i + ' '
+print(count)
+
+
